101f7f15a0af9c210568cdf3cf14bf8e62221f41
Ronald Campbell <ronaldcampbell@id.uff.br>
2018-10-31 22:57:41 -0300

    todos os possiveis movimentos com tratamentos

--- movimentos_pecas.py
+++ movimentos_pecas.py
@@ -16,7 +16,7 @@ def movimento_obrigatorio_torre(tabuleiro,x,y):
     i=x+1
     while(i < len(tabuleiro)):
         listaPossiveis.append([i,y])
-        if (tabuleiro[i][y] !=0):
+        if (tabuleiro[i][y] !='0'):
             if(isPecaAliada(tabuleiro,x,y,i,y)): 
                 listaPossiveis.remove([i,y])
             break
@@ -24,15 +24,15 @@ def movimento_obrigatorio_torre(tabuleiro,x,y):
     i = x-1
     while(i>=0):
         listaPossiveis.append([i,y])
-        if (tabuleiro[i][y] !=0):
+        if (tabuleiro[i][y] !='0'):
             if(isPecaAliada(tabuleiro,x,y,i,y)): 
                 listaPossiveis.remove([i,y])
             break
         i-=1
     j = y+1
-    while(j < len(tabuleiro)[0]):
+    while(j < len(tabuleiro[0])):
         listaPossiveis.append([x,j])
-        if (tabuleiro[x][j] !=0):
+        if (tabuleiro[x][j] !='0'):
             if(isPecaAliada(tabuleiro,x,y,x,j)): 
                 listaPossiveis.remove([x,j])
             break
@@ -40,7 +40,7 @@ def movimento_obrigatorio_torre(tabuleiro,x,y):
     j = y-1
     while(j >= 0):
         listaPossiveis.append([x,j])
-        if (tabuleiro[x][j] !=0):
+        if (tabuleiro[x][j] !='0'):
             if(isPecaAliada(tabuleiro,x,y,x,j)): 
                 listaPossiveis.remove([x,j])
             break
@@ -51,10 +51,9 @@ def movimento_obrigatorio_bispo(tabuleiro,x,y):
     listaPossiveis = []
     i=x+1
     j=y+1
-    while(i < len(tabuleiro) and j < len(tabuleiro)[0]):
-        #Verificar se possui peças na frente
+    while(i < len(tabuleiro) and j < len(tabuleiro[0])):
         listaPossiveis.append([i,j])
-        if (tabuleiro[i][j] !=0):
+        if (tabuleiro[i][j] != '0'):
             if(isPecaAliada(tabuleiro,x,y,i,j)): 
                 listaPossiveis.remove([i,j])
             break
@@ -65,7 +64,7 @@ def movimento_obrigatorio_bispo(tabuleiro,x,y):
     j = y-1
     while(i>=0 and j>=0):
         listaPossiveis.append([i,j])
-        if (tabuleiro[i][j] !=0):
+        if (tabuleiro[i][j] != '0'):
             if(isPecaAliada(tabuleiro,x,y,i,j)): 
                 listaPossiveis.remove([i,j])
             break
@@ -74,9 +73,9 @@ def movimento_obrigatorio_bispo(tabuleiro,x,y):
 
     i = x-1
     j = y+1
-    while(j < len(tabuleiro)[0] and i>=0):
+    while(j < len(tabuleiro[0]) and i>=0):
         listaPossiveis.append([i,j])
-        if (tabuleiro[i][j] !=0):
+        if (tabuleiro[i][j] != '0'):
             if(isPecaAliada(tabuleiro,x,y,i,j)): 
                 listaPossiveis.remove([i,j])
             break
@@ -87,7 +86,7 @@ def movimento_obrigatorio_bispo(tabuleiro,x,y):
     j = y-1
     while(j >= 0 and i <len(tabuleiro)):
         listaPossiveis.append([i,j])
-        if (tabuleiro[i][j] !=0):
+        if (tabuleiro[i][j] != '0'):
             if(isPecaAliada(tabuleiro,x,y,i,j)): 
                 listaPossiveis.remove([i,j])
             break
@@ -105,3 +104,167 @@ def movimento_obrigatorio_rainha(tabuleiro,x,y):
 
     return listaPossiveisRainha 
 
+def movimento_obrigatorio_rei(tabuleiro,x,y):
+    listaPossiveis = []
+    i = x+1
+    j = y+1
+    if(i<len(tabuleiro) and j<len(tabuleiro[0])):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    i = x-1
+    j = y-1
+    if(i>=0 and j>=0):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    i = x-1
+    j = y+1
+    if(i>=0 and j<len(tabuleiro[0])):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+    
+    i = x+1
+    j = y-1
+    if(i<len(tabuleiro) and j>=0):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    i = x+1
+    j = y
+    if(i<len(tabuleiro)):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    i = x
+    j = y+1
+    if(j<len(tabuleiro)):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+        
+    i = x-1
+    j = y
+    if(i>=0):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    i = x
+    j = y-1
+    if(j>=0):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    return listaPossiveis
+
+def movimento_obrigatorio_peao(tabuleiro,x,y):
+    listaPossiveis = []
+    if(tabuleiro[x][y].isupper()):
+        i = x+1
+        j = y
+        if(i<len(tabuleiro)):
+            if(tabuleiro[i][j] == '0'):
+                listaPossiveis.append([i,j])
+        if(x<len(tabuleiro) and y <len(tabuleiro[0]) and tabuleiro[i][j] != '0'):
+            if(isPecaAliada(tabuleiro,x,y,x+1,y+1)):
+                listaPossiveis.append([i,j])
+        if(x<len(tabuleiro) and y >=0 and tabuleiro[i][j] != '0'):
+            if(isPecaAliada(tabuleiro,x,y,x+1,y-1)):
+                listaPossiveis.append([i,j])
+    else:
+        if(tabuleiro[x][y].islower()):
+        i = x-1
+        j = y
+        if(i<len(tabuleiro)):
+            if(tabuleiro[i][j] == '0'):
+                listaPossiveis.append([i,j])
+        if(x>=0 and y <len(tabuleiro[0]) and tabuleiro[i][j] != '0'):
+            if(isPecaAliada(tabuleiro,x,y,x+1,y+1)):
+                listaPossiveis.append([i,j])
+        if(x>=0 and y >=0 and tabuleiro[i][j] != '0'):
+            if(isPecaAliada(tabuleiro,x,y,x+1,y-1)):
+                listaPossiveis.append([i,j])
+    return listaPossiveis
+
+def movimento_obrigatorio_cavalo(tabuleiro,x,y):
+    listaPossiveis = []
+    i = x+1
+    j = y+2
+    if(i<len(tabuleiro) and j<len(tabuleiro[0])):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    i = x-1
+    j = y-2
+    if(i>=0 and j>=0):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    i = x-1
+    j = y+2
+    if(i>=0 and j<len(tabuleiro[0])):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+    
+    i = x+1
+    j = y-2
+    if(i<len(tabuleiro) and j>=0):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+                
+    i = x+2
+    j = y+1
+    if(i<len(tabuleiro) and j<len(tabuleiro[0])):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    i = x-2
+    j = y-1
+    if(i>=0 and j>=0):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    i = x-2
+    j = y+1
+    if(i>=0 and j<len(tabuleiro[0])):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+    
+    i = x+2
+    j = y-1
+    if(i<len(tabuleiro) and j>=0):
+        listaPossiveis.append([i,j])
+        if(tabuleiro[i][j] != '0'):
+           if(isPecaAliada(tabuleiro,x,y,i,j)): 
+                listaPossiveis.remove([i,j])
+
+    return listaPossiveis
